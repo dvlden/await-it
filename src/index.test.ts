843ec77158f2d541c2eb0a2b57dfad5bd1fb6433
Nenad Novakovic <dvlden@users.noreply.github.com>
2023-01-23 18:53:22 +0100

    fix: use optional error type and nulls in response

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,23 +1,25 @@
-import { it } from '../src/index'
+import { describe, expect, test } from 'vitest'
+import { it } from './index'
 
 describe('index.ts', () => {
   const resolve = { status: true }
   const reject = new Error('Give me a reason.')
-  const defaultReject = new Error('Rejection reason unspecified.')
 
   describe('result', () => {
     test('result should contain what promise resolves', async () => {
       const [res1] = await it(Promise.resolve(resolve))
-      const [res2] = await it(Promise.all([Promise.resolve('one'), Promise.resolve('two')]))
+      const [res2] = await it(
+        Promise.all([Promise.resolve('one'), Promise.resolve('two')]),
+      )
 
       expect(res1).toEqual(resolve)
       expect(res2).toEqual(['one', 'two'])
     })
 
-    test('result should be `undefined` on rejection', async () => {
+    test('result should be `null` on rejection', async () => {
       const [res] = await it(Promise.reject())
 
-      expect(res).toBeUndefined()
+      expect(res).toBeNull()
     })
   })
 
@@ -26,13 +28,6 @@ describe('index.ts', () => {
       const [_, err] = await it(Promise.resolve(resolve))
 
       expect(err).toBeNull()
-    })
-
-    test('error should be `default` on rejection without a message', async () => {
-      const [_, err] = await it(Promise.reject())
-
-      expect(err).toBeInstanceOf(Error)
-      expect(err!.message).toBe(defaultReject.message)
     })
 
     test('error should be what promise rejects', async () => {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 type Ok<T> = Readonly<[T, null]>
-type Err<T> = Readonly<[undefined, T]>
+type Err<T> = Readonly<[null, T]>
 type Result<O, E> = Promise<Ok<O> | Err<E>>
 
-export const it = <T>(
-  promise: Promise<T>
-): Result<T, Error> => {
+export const it = <T, F = Error>(promise: Promise<T>): Result<T, F> => {
   return promise
-    .then<Ok<T>>(
-      (res: T) => [res, null]
-    )
-    .catch<Err<Error>>(
-      (err?: Error) => [undefined, err || new Error('Rejection reason unspecified.')]
-    )
+    .then<Ok<T>>((res: T) => [res, null])
+    .catch<Err<F>>((err: F) => [null, err])
 }
